d3c50314e95b8f68298b5f048dbc7b52f2b10710
wupeng77 <1217558119@qq.com>
2019-05-07 15:33:16 +0800

    添加日志 ，失败了 commit;

--- src/main/java/com/cn/controller/UserController.java
+++ src/main/java/com/cn/controller/UserController.java
@@ -2,6 +2,8 @@ package com.cn.controller;
 
 import com.cn.dto.Suser;
 import com.cn.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private UserService  userService;
     @Autowired
@@ -24,12 +29,15 @@ public class UserController {
     public ModelAndView getUserInfo(String userId){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
-
+        logger.info("info--日志开始");
         Suser user = userService.getUserInfo(userId);
 //        strRedis.opsForSet().add("1234","1234");
         redisTemplate.opsForValue().set(userId,userId);
         System.out.print(strRedis.opsForValue().get("wpeng")+"---");
         mv.addObject(user);
+        logger.info("info--日志结束");
+        logger.warn("警告！");
+        logger.debug("debug!");
         return  mv;
     }
 }
--- src/main/java/com/cn/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/cn/service/serviceimpl/UserServiceImpl.java
@@ -3,21 +3,27 @@ package com.cn.service.serviceimpl;
 import com.cn.dao.SuserMapper;
 import com.cn.dto.Suser;
 import com.cn.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class UserServiceImpl implements UserService {
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private SuserMapper suserMapper;
 
     public Suser getUserInfo(String userId){
         Suser user = null;
         try {
-            System.out.print(suserMapper);
             user = suserMapper.selectByPrimaryKey(userId);
+            logger.info(user.getUsername());
         }catch (Exception E){
-            E.printStackTrace();
+            logger.error(E.getMessage());
+            throw E;
         }
         return  user;
     }
