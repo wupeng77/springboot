d4c1aab33513b311ae35610bbb2964612a4bb310
wupeng77 <1217558119@qq.com>
2019-05-07 11:37:59 +0800

    添加html跳转 commit

--- src/main/java/com/cn/controller/UserController.java
+++ src/main/java/com/cn/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/user")
@@ -20,11 +21,15 @@ public class UserController {
     private RedisTemplate  redisTemplate;
 
     @GetMapping("/login")
-    public Suser getUserInfo(String userId){
+    public ModelAndView getUserInfo(String userId){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+
         Suser user = userService.getUserInfo(userId);
 //        strRedis.opsForSet().add("1234","1234");
         redisTemplate.opsForValue().set(userId,userId);
         System.out.print(strRedis.opsForValue().get("wpeng")+"---");
-        return  user;
+        mv.addObject(user);
+        return  mv;
     }
 }
