cbf74a3dcd9dcffd28ca07e6e5269f22be3b77c5
wupeng77 <1217558119@qq.com>
2019-05-13 23:37:39 +0800

    eureka-client

--- src/main/java/com/cn/controller/UserController.java
+++ src/main/java/com/cn/controller/UserController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,37 +16,39 @@ public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
-    private UserService  userService;
+    private UserService userService;
     @Autowired
-    private RedisTemplate  redisTemplate;
+    private RedisTemplate redisTemplate;
 
     /**
      * 准备登陆页面
+     *
      * @param userId
      * @return
      */
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public ModelAndView getUserInfo(String userId){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getUserInfo(String userId) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
-        logger.info("mv",mv);
-        return  mv;
+        logger.info("mv", mv);
+        return mv;
     }
 
     /**
      * 登陆检验
+     *
      * @param userDto
      * @return
      */
-    @RequestMapping(value = "/loginCheck",method = RequestMethod.POST)
-    public ModelAndView loginCheck(@ModelAttribute Suser userDto){
+    @RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
+    public ModelAndView loginCheck(@ModelAttribute Suser userDto) {
 
         ModelAndView mv = new ModelAndView();
         boolean loginFlag = userService.loginCheck(userDto);
 
-        if (loginFlag){
+        if (loginFlag) {
             mv.setViewName("success");
-        }else {
+        } else {
             mv.setViewName("index");
         }
         return mv;
@@ -55,19 +56,32 @@ public class UserController {
 
     /**
      * 注册用户
+     *
      * @param userDto
      * @return
      */
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public ModelAndView registerUser(@ModelAttribute Suser userDto){
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView registerUser(@ModelAttribute Suser userDto) {
         ModelAndView mv = new ModelAndView();
         int successFlag = userService.insertUser(userDto);
 
-        if ("1".equals(successFlag)){
+        if ("1".equals(successFlag)) {
             mv.setViewName("success");
-        }else {
+        } else {
             mv.setViewName("index");
         }
+        return mv;
+    }
+
+    /**
+     * 准备注册
+     *
+     * @return
+     */
+    @RequestMapping("/prepRegister")
+    public ModelAndView prepRegister() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("register");
         return mv;
     }
 }
--- src/main/java/com/cn/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/cn/service/serviceimpl/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.cn.dao.SuserMapper;
 import com.cn.dto.Suser;
 import com.cn.dto.SuserExample;
 import com.cn.service.UserService;
-import com.sun.tools.internal.xjc.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
